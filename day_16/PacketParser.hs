d8afda6858294bd9a645cbbd93aee67d7edf941f
Sebastian Woelke <sebastian.woelke@posteo.de>
2021-12-22 20:15:54 +0100

    day 16 part 2

--- day_16/PacketParser.hs
+++ day_16/PacketParser.hs
@@ -10,14 +10,17 @@ import Data.Maybe
 -- import qualified Data.Map as Map
 -- import qualified Data.Set as Set
 
+data OperatorType = Sum | Prod | Min | Max | Value | Gthan | Lthan | Equal deriving (Eq, Show, Enum)
 data Content = Val Integer
-             | Op Integer [Packet] deriving Show
+             | Op OperatorType [Packet] deriving Show
 data Packet = Ver Integer Content deriving Show
 
 type HexChar = Char
 type HexString = String
 type BinChar = Char
 type BinString = String
+
+
 
 hexChar2BinStr :: HexChar -> Maybe BinString
 hexString2BinStr :: HexString -> Maybe BinString
@@ -27,9 +30,10 @@ binChar2Bool :: BinChar -> Maybe Bool
 parsePacket :: BinString -> (Packet, BinString)
 parseContent :: BinString -> (Content, BinString)
 parseLiteralValue :: BinString -> (Content, BinString)
-parseSubPackets :: Integer -> BinString -> (Content, BinString)
+parseSubPackets :: OperatorType -> BinString -> (Content, BinString)
 
 versionsToList :: Packet -> [Integer]
+calcPacketValue :: Packet -> Integer
 
 run1 :: IO ()
 
@@ -80,14 +84,14 @@ parseLiteralValue binStr = let (binStrVal, remainder) = parseGroups (fromJust $ 
                                   (nextBinStrVal, remainder) = parseGroups (fromJust $ binChar2Bool $ xs !! 4) (drop 5 xs)
                                in (binStrVal ++ nextBinStrVal, remainder)
 
-parseContent binStr = let (typeId, remainder) = parseTypeId binStr
-                       in dispatch typeId remainder
-  where parseTypeId ys = (fromJust $ binStr2Integer $ take 3 ys, drop 3 ys)
-        dispatch typeId ys
-          | typeId == 4 = parseLiteralValue ys
-          | otherwise = parseSubPackets typeId ys
+parseContent binStr = let (opType, remainder) = parseOperatorType binStr
+                       in dispatch opType remainder
+  where parseOperatorType ys = (toEnum $ fromIntegral $ fromJust $ binStr2Integer $ take 3 ys, drop 3 ys)
+        dispatch opType ys
+          | opType == Value = parseLiteralValue ys
+          | otherwise = parseSubPackets opType ys
 
-parseSubPackets typeId binStr = dispatchFunByLengthTypeId $ tail binStr
+parseSubPackets opType binStr = dispatchFunByLengthTypeId $ tail binStr
   where dispatchFunByLengthTypeId
           | head binStr == '0' = parseByLength
           | head binStr == '1' = parseByCount
@@ -100,13 +104,13 @@ parseSubPackets typeId binStr = dispatchFunByLengthTypeId $ tail binStr
                                                 (\(_, r) _ -> not $ null r)
                                                 (\(ps, r) _ -> let (p, r1) = parsePacket r in (ps ++ [p], r1))
                                                 ([] , toParseRemainder) [(1::Integer)..]
-                            in (Op typeId packets, notParsedRemainder)
+                            in (Op opType packets, notParsedRemainder)
         parseByCount xs = let count = fromJust $ binStr2Integer $ take 11 xs
                               wholeRemainder = drop 11 xs
                               (packets, notParsedRemainder) = foldl
                                                                 (\(ps, r) _ -> let (p, r1) = parsePacket r in (ps ++ [p], r1))
                                                                 ([], wholeRemainder) [1..count]
-                           in (Op typeId packets, notParsedRemainder)
+                           in (Op opType packets, notParsedRemainder)
 
 parsePacket binStr = let (version, remainder) = parseVersion binStr
                          (content, remainder1) = parseContent remainder
@@ -117,9 +121,22 @@ versionsToList (Ver ver content) = ver : concat ( versfromContent content)
   where versfromContent (Val _) =  []
         versfromContent (Op _ packets) = map versionsToList packets
 
+calcPacketValue (Ver _ content) = value content
+  where values ps = map calcPacketValue ps
+        value (Val x) = x
+        value (Op Sum ps) = sum $ values ps
+        value (Op Prod ps) = product $ values ps
+        value (Op Min ps) = minimum $ values ps
+        value (Op Max ps) = maximum $ values ps
+        value (Op Gthan ps) = let [a,b] = values ps in fromIntegral $ fromEnum (a > b)
+        value (Op Lthan ps) = let [a,b] = values ps in fromIntegral $ fromEnum (a < b)
+        value (Op Equal ps) = let [a,b] = values ps in fromIntegral $ fromEnum (a == b)
+        value _ = error "unknown operator"
+
 run1 =
   do ls <- readLines "puzzle.txt"
      let p = parsePacket $ fromJust $ hexString2BinStr $ head ls
      print p
-     print $ sum $ versionsToList $ fst p
+     -- print $ sum $ versionsToList $ fst p
+     print $ calcPacketValue $ fst p
 
